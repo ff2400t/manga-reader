f036d3da1b209773cd2c9af71ea5a94fa6a16a5b
ff2400t <84376771+ff2400t@users.noreply.github.com>
2023-05-21 20:58:59 +0530

    added the vertical mode

--- src/manga-reader.ts
+++ src/manga-reader.ts
@@ -1,5 +1,8 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import { classMap } from 'lit/directives/class-map.js';
+
+type mode = 'horizontal' | 'vertical'
 
 /**
  * Manga Reader component
@@ -10,9 +13,16 @@ export class MangaReader extends LitElement {
   @property()
   pages: string[] = [];
 
+  @property()
+  mode: mode = 'horizontal'
+
   render() {
+    const classes = {
+      horizontal: this.mode === 'horizontal',
+      vertical: this.mode === 'vertical'
+    }
     return html`
-        <div class='container'>
+        <div class='container ${classMap(classes)}'>
           ${this.pages.map(url => html`
         <div class='page'>
           <img src=${url} />
@@ -27,15 +37,19 @@ export class MangaReader extends LitElement {
     overflow: hidden;
   }
 
-  .container {
+  .container{
+    container: mr / inline-size; 
     display: grid;
+  }
+
+  .container.horizontal {
     overflow-x: scroll;
     scroll-snap-type: x mandatory;
     grid-auto-flow: column;
     grid-auto-columns: 100vw;
   }
 
-  .page {
+  .horizontal .page {
     scroll-snap-align: center;
     width: 100vw;
     height: 100vh;
@@ -44,11 +58,29 @@ export class MangaReader extends LitElement {
     justify-content: center;
   }
   
-  .page img {
+  .horizontal .page img {
     display: block;
     width: auto;
     height: 100%;
   }
+
+  .container.vertical {
+    width: 100vw;
+    justify-items: center
+  }
+
+  .vertical .page {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .vertical .page img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+
     
   `
 }
