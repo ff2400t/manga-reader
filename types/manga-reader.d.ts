7f892eb759b5ed89a7adce569bb2c9a55e7f4157
ff2400t <84376771+ff2400t@users.noreply.github.com>
2023-05-28 17:08:44 +0530

    updated the types file

--- types/manga-reader.d.ts
+++ types/manga-reader.d.ts
@@ -1,5 +1,6 @@
 import { LitElement, PropertyValueMap, PropertyValues } from 'lit';
-type Mode = 'horizontal-rtl' | 'horizontal-ltr' | 'vertical';
+type Mode = 'horizontal' | 'vertical' | 'double-page' | 'double-page-odd' | 'webtoon';
+type ReadingDirection = 'rtl' | 'ltr';
 type ScaleType = 'fitWidth' | 'fitHeight';
 /**
  * Manga Reader component
@@ -8,19 +9,33 @@ export declare class MangaReader extends LitElement {
     #private;
     pages: string[];
     mode: Mode;
+    dir: ReadingDirection;
     currentPage: number;
     scaleType: ScaleType;
-    container: HTMLDivElement;
+    handleMiddleClick: () => void;
     /**
-     * Amount to scroll during a click event in vertical Mode
+     * No of images to preload after the current Image
+     * The default is to preload the next page
+     */
+    preloadNo: number;
+    container: HTMLDivElement;
+    touchIndicator: HTMLDivElement;
+    doublePagedArr: Array<{
+        url: string;
+        index: number;
+    }[]>;
+    /**
+     * Amount to scroll during a click event in webtoon Mode
      * Number between 0 and 1
      */
-    verticalScrollAmount: number;
+    webtoonScrollAmount: number;
     observer: IntersectionObserver;
     connectedCallback(): void;
     disconnectedCallback(): void;
     shouldUpdate(changedProperties: PropertyValues<this>): boolean;
-    attributeChangedCallback(name: string, _: string, newValue: string): void;
+    willUpdate(changedProperties: PropertyValues<this>): void;
+    attributeChangedCallback(name: string, oldValue: string, newValue: string): void;
+    firstUpdated(): void;
     updated(changedProperties: PropertyValueMap<any>): void;
     render(): import("lit").TemplateResult<1>;
     /**
@@ -29,7 +44,8 @@ export declare class MangaReader extends LitElement {
      */
     gotoPage(num: number): boolean;
     setUpHorizontalIntersectionObserver(): void;
-    setUpVerticalIntersectionObserver(): void;
+    setUpWebtoonIntersectionObserver(): void;
+    showTouchIndicator(duration?: number): void;
     static styles: import("lit").CSSResult;
 }
 declare global {
