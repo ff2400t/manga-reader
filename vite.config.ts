d8c64f8b1f249cefa8f5204c1da48569894b4d14
ff2400t <84376771+ff2400t@users.noreply.github.com>
2023-05-29 13:50:58 +0530

    added two different build output options

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/manga-reader.ts',
-      formats: ['es'],
-    },
-    rollupOptions: {
-      // external: /^lit/
-    },
-  },
+export default defineConfig(() => {
+  // this is to make the library build of manga-reader;
+  // i haven't found a way to make it work from npm scripts
+  // so for now i have to manually set the env
+  // `BUILD_MODE='lib' pnpm run build`
+  if (process.env.BUILD_MODE === 'lib') {
+    return {
+      build: {
+        lib: {
+          entry: 'src/manga-reader.ts',
+          formats: ['es'],
+        },
+        rollupOptions: {
+          // external: /^lit/
+        },
+      },
+    } 
+  }
+  return {}
 })
+
