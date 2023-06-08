6b696dc226e49d76a04db154d679f68350973f7c
ff2400t <84376771+ff2400t@users.noreply.github.com>
2023-06-08 16:52:51 +0530

    added a demo page and added the images

---
+++ demo/index.js
@@ -0,0 +1,28 @@
+import { createApp } from 'https://unpkg.com/petite-vue?module' 
+
+const reader = document.querySelector('manga-reader');
+const controls = document.querySelector('.controls');
+
+reader.pages = Array.from({length: 9}).map((_, i) => `/pages/00${i + 1}.jpg`)
+reader.handleMiddleClick = () => controls.classList.toggle('open')
+reader.preloadNo = 3
+
+reader.addEventListener('pagechange', (e) => console.log(e)) 
+
+createApp({
+  get mode() { return reader.mode },
+  set mode(newValue) { reader.mode = newValue },
+
+  get dir() { return reader.dir },
+  set dir(newValue) { reader.dir = newValue },
+
+  get scaleType() { return reader.scaleType },
+  set scaleType(newValue) { reader.scaleType = newValue }, 
+
+  showTouchArea: false,
+  showTouchIndicator() {
+    console.log(this.showTouchArea)
+    if (!this.showTouchArea) reader.hideTouchIndicator();
+    else reader.showTouchIndicator()
+  }
+}).mount()
