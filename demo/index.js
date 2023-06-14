64e409f3762c624f5a92d6ba047b0af563ed6508
ff2400t <84376771+ff2400t@users.noreply.github.com>
2023-06-14 18:04:51 +0530

    updated the demo by adding localstorage for the options

--- demo/index.js
+++ demo/index.js
@@ -3,23 +3,51 @@ import { createApp } from 'https://unpkg.com/petite-vue?module'
 const reader = document.querySelector('manga-reader');
 const controls = document.querySelector('.controls');
 
-reader.pages = Array.from({ length: 9 }).map((_, i) => `/pages/00${i + 1}.jpg`)
-reader.handleMiddleClick = () => controls.classList.toggle('open')
-reader.preloadNo = 3
+const saveMode = localStorage.getItem('--mr-mode')
+const saveDir = localStorage.getItem('--mr-dir')
+const saveScale = localStorage.getItem('--mr-scale')
+const savePadding = localStorage.getItem('--mr-padding')
+reader.preloadNo = 3;
+if (saveMode) {
+  reader.mode = saveMode
+}
+if (saveDir) {
+  reader.dir = saveDir
+}
+if (saveScale) {
+  reader.scaleType = saveScale
+}
+if (savePadding) {
+  reader.webtoonPadding = savePadding
+}
 
-reader.addEventListener('pagechange', (e) => console.log(e))
+reader.handleMiddleClick = () => controls.classList.toggle('open');
 
 createApp({
   get mode() { return reader.mode },
-  set mode(newValue) { reader.mode = newValue },
+  set mode(newValue) {
+    reader.mode = newValue
+    localStorage.setItem('--mr-mode', newValue)
+  },
 
   get dir() { return reader.dir },
-  set dir(newValue) { reader.dir = newValue },
+  set dir(newValue) {
+    reader.dir = newValue
+    localStorage.setItem('--mr-dir', newValue)
+  },
 
   get scaleType() { return reader.scaleType },
-  set scaleType(newValue) { reader.scaleType = newValue },
+  set scaleType(newValue) {
+    reader.scaleType = newValue
+    localStorage.setItem('--mr-scale', newValue)
+  },
 
   get showTouchIndicator() { return reader.showTouchIndicator },
   set showTouchIndicator(newValue) { reader.showTouchIndicator = newValue },
+
+  get webtoonPadding() { return reader.webtoonPadding },
+  set webtoonPadding(newValue) {
+    reader.webtoonPadding = newValue
+  },
 
 }).mount()
